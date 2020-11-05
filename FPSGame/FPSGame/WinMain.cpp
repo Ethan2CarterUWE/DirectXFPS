086e655a20f36997b83a4e11f5512f3a42a81021
Ethan <Ethan2.Carter@live.uwe.ac.uk>
2020-11-05 00:37:21 +0000

    ok

--- FPSGame/FPSGame/WinMain.cpp
+++ FPSGame/FPSGame/WinMain.cpp
@@ -4,26 +4,35 @@
 
 #include <windows.h>
 #include <windowsx.h>
-#include <d3d9.h>
-
+#include <d3d11.h>
 
 #include<string>
 #include<sstream>
 
 
-
-
 // include the Direct3D Library file
-#pragma comment (lib, "d3d9.lib")
+#pragma comment (lib, "d3d11.lib")
+
+// define the screen resolution
+#define SCREEN_WIDTH  800
+#define SCREEN_HEIGHT 600
 
 // global declarations
-LPDIRECT3D9 d3d;    // the pointer to our Direct3D interface
-LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
+IDXGISwapChain* swapchain;             // the pointer to the swap chain interface
+ID3D11Device* dev;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext* devcon;           // the pointer to our Direct3D device context
+ID3D11RenderTargetView* backbuffer;    // the pointer to our back buffer
+
 
 // function prototypes
-void initD3D(HWND hwnd);    // sets up and initializes Direct3D
-void render_frame(void);    // renders a single frame
-void cleanD3D(void);    // closes Direct3D and releases memory
+void InitD3D(HWND hwnd);    // sets up and initializes Direct3D
+void RenderFrame(void);     // renders a single frame
+void CleanD3D(void);        // closes Direct3D and releases memory
+
+// function prototypes
+//void initD3D(HWND hwnd);    // sets up and initializes Direct3D
+//void render_frame(void);    // renders a single frame
+//void cleanD3D(void);    // closes Direct3D and releases memory
 
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -51,7 +60,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         WS_OVERLAPPEDWINDOW,     // window style
 
         //  position(2) and size(2) 
-        CW_USEDEFAULT, CW_USEDEFAULT, 1440, 900,
+        0, 0, SCREEN_WIDTH, SCREEN_HEIGHT,
 
         NULL,       // no parent window    
         NULL,       // no menus
@@ -67,8 +76,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     //display window on screen
     ShowWindow(hwnd, nCmdShow);
 
-    initD3D(hwnd);
-
+    InitD3D(hwnd);
     // Run the message loop.
 
     MSG msg;
@@ -88,61 +96,101 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
         if (msg.message == WM_QUIT)
             break;
-
-        render_frame();
+        RenderFrame();
     }
        
     
       
 
     
-    cleanD3D();
+    CleanD3D();
     return 0;
 
   
 }
 
-void initD3D(HWND hwnd)
+// this function initializes and prepares Direct3D for use
+void InitD3D(HWND hWnd)
 {
-    d3d = Direct3DCreate9(D3D_SDK_VERSION);    // create the Direct3D interface
+    // create a struct to hold information about the swap chain
+    DXGI_SWAP_CHAIN_DESC scd;
 
-    D3DPRESENT_PARAMETERS d3dpp;    // create a struct to hold various device information
+    // clear out the struct for use
+    ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
 
-    ZeroMemory(&d3dpp, sizeof(d3dpp));    // clear out the struct for use
-    d3dpp.Windowed = TRUE;    // program windowed, not fullscreen
-    d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;    // discard old frames
-    d3dpp.hDeviceWindow = hwnd;    // set the window to be used by Direct3D
+    // fill the swap chain description struct
+    scd.BufferCount = 1;                                    // one back buffer
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+    scd.BufferDesc.Width = SCREEN_WIDTH;                    // set the back buffer width
+    scd.BufferDesc.Height = SCREEN_HEIGHT;                  // set the back buffer height
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+    scd.OutputWindow = hWnd;                                // the window to be used
+    scd.SampleDesc.Count = 4;                               // how many multisamples
+    scd.Windowed = TRUE;                                    // windowed/full-screen mode
+    scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;     // allow full-screen switching
 
-    // create a device class using this information and information from the d3dpp stuct
-    d3d->CreateDevice(D3DADAPTER_DEFAULT,
-        D3DDEVTYPE_HAL,
-        hwnd,
-        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-        &d3dpp,
-        &d3ddev);
+    // create a device, device context and swap chain using the information in the scd struct
+    D3D11CreateDeviceAndSwapChain(NULL,
+        D3D_DRIVER_TYPE_HARDWARE,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        D3D11_SDK_VERSION,
+        &scd,
+        &swapchain,
+        &dev,
+        NULL,
+        &devcon);
+
+    // get the address of the back buffer
+    ID3D11Texture2D* pBackBuffer;
+    swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+    // use the back buffer address to create the render target
+    dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+    pBackBuffer->Release();
+
+    // set the render target as the back buffer
+    devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+
+    // Set the viewport
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = SCREEN_WIDTH;
+    viewport.Height = SCREEN_HEIGHT;
+
+    devcon->RSSetViewports(1, &viewport);
 }
 
 // this is the function used to render a single frame
-void render_frame(void)
+void RenderFrame(void)
 {
-    // clear the window to a deep blue
-    d3ddev->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 40, 100), 1.0f, 0);
-
-    d3ddev->BeginScene();    // begins the 3D scene
-
+    // clear the back buffer to a deep blue
+    float color[4] = { 0.0f, 0.3f, 0.4f, 1.0f };
+    devcon->ClearRenderTargetView(backbuffer, color);
     // do 3D rendering on the back buffer here
 
-    d3ddev->EndScene();    // ends the 3D scene
-
-    d3ddev->Present(NULL, NULL, NULL, NULL);    // displays the created frame
+    // switch the back buffer and the front buffer
+    swapchain->Present(0, 0);
 }
 
 // this is the function that cleans up Direct3D and COM
-void cleanD3D(void)
+void CleanD3D(void)
 {
-    d3ddev->Release();    // close and release the 3D device
-    d3d->Release();    // close and release Direct3D
+    swapchain->SetFullscreenState(FALSE, NULL);    // switch to windowed mode
+    // close and release all existing COM objects
+    swapchain->Release();
+    backbuffer->Release();
+    dev->Release();
+    devcon->Release();
 }
+
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
